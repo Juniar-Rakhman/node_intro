1ddd5be3e99432c8c577a1f6a1f2d0a7d9b64813
juniar_rakhman <juniar.rahman@gmail.com>
2017-02-25 16:31:35 +0700

    initial commit

--- node_1/1-hello.js
+++ node_1/1-hello.js
@@ -1,8 +1,8 @@
 var http = require("http");
 
-http.createServer(function(req, res){
+http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end("Hello there");
-}).listen(process.env.PORT, process.env.IP);
+}).listen('3000', '127.0.0.1');
 
 console.log("Server is running...");
